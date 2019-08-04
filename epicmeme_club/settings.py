3bdb223f9d51edb1e0a94550866858805a849c47
Lumiobyte <evanpartridge0@gmail.com>
2019-08-04 11:49:54 +1000

    changing static settings

--- epicmeme_club/settings.py
+++ epicmeme_club/settings.py
@@ -133,12 +133,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = 'static' # live cdn such as AWS S3
+MEDIA_URL = '/memes/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 
-
-MEDIA_ROOT = os.path.join(STATIC_ROOT, 'media')
-MEDIA_URL = '/memes/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media_root')
